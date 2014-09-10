95a77bd7f18d72ef5e35b4ff21162516831bb08a
Nate Fox <nfox@byu.edu>
2014-09-10 10:03:36 -0600

    fixed 2e

--- 2e.py
+++ 2e.py
@@ -3,7 +3,7 @@ import sys
 values = [57, 71, 87, 97, 99, 101, 103, 113, 114, 115, 128, 129, 131, 137, 147, 156, 163, 186]
 
 def sequence_cyclopeptide(spectrum):
-	spectrum_mass = spectrum[-1] # TODO is this right?
+	spectrum_mass = spectrum[-1]
 	queue = set([tuple([0])])
 	solution = []
 	while queue:
@@ -11,9 +11,8 @@ def sequence_cyclopeptide(spectrum):
 		prune = set()
 		for peptide in queue:
 			if sum(peptide) == spectrum_mass:
-				#print peptide, get_spectrum(peptide), spectrum
-				#if get_spectrum(peptide) == spectrum:
-				solution.append(peptide)
+				if get_spectrum(peptide, True) == spectrum:
+					solution.append(peptide)
 				prune.add(peptide)
 			elif not is_consistent(peptide, spectrum):
 				prune.add(peptide)
@@ -27,9 +26,9 @@ def sequence_cyclopeptide(spectrum):
 def expand(queue):
 	return set([peptide + tuple([value]) for peptide in queue for value in values])
 
-def is_consistent(peptide, spectrum): # TODO is this right?
+def is_consistent(peptide, spectrum):
 	spec_copy = list(spectrum)
-	pep_spectrum = get_spectrum(peptide)
+	pep_spectrum = get_spectrum(peptide, False)
 	for mass in pep_spectrum:
 		try:
 			spec_copy.remove(mass)
@@ -37,10 +36,10 @@ def is_consistent(peptide, spectrum): # TODO is this right?
 			return False
 	return True
 
-def get_spectrum(peptide):
-	return sorted(map(sum, get_subpeptides(peptide)))
+def get_spectrum(peptide, isCircular):
+	return sorted(map(sum, get_subpeptides(peptide, isCircular)))
 
-def get_subpeptides(peptide): # TODO is this right?
+def get_subpeptides(peptide, isCircular):
 	if peptide[0] == 0:
 		peptide = peptide[1:]
 	subs = [(0,)]
@@ -48,30 +47,12 @@ def get_subpeptides(peptide): # TODO is this right?
 		for i in xrange(len(peptide)):
 			if i+length <= len(peptide):
 				subs.append(peptide[i:i+length])
-			"""
-			else: # TODO I took this branch out and that seemed to make the example on page 60 run better
+			elif isCircular:
 				rem = length - (len(peptide) - i)
 				subs.append(peptide[i:] + peptide[0:rem])
-			"""
 	subs.append(peptide)
 	return subs
 
 #spectrum = map(int, "0 97 97 99 101 103 196 198 198 200 202 295 297 299 299 301 394 396 398 400 400 497".split())
 spectrum = [map(int, line.split()) for line in open("input.txt", "r")][0]
 print ' '.join(map(lambda peptide: '-'.join(map(str, peptide[1:])), sequence_cyclopeptide(spectrum)))
-
-"""
-print get_subpeptides((0,1,))
-print get_subpeptides((0,1,2))
-print get_subpeptides((0,1,2,3))
-print get_subpeptides((0,1,2,1))
-print get_subpeptides((0,1,1,1))
-print get_subpeptides((0,1,2,1,2))
-
-print get_spectrum((0,1,))
-print get_spectrum((0,1,2))
-print get_spectrum((0,1,2,3))
-print get_spectrum((0,1,2,1))
-print get_spectrum((0,1,1,1))
-print get_spectrum((0,1,2,1,2))
-"""
