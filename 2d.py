e7e0a17ee34cd65850a1bbc615eada9867509695
Nate Fox <nfox@byu.edu>
2014-09-16 14:38:29 -0600

    going back to work on 2d a bit

--- 2d.py
+++ 2d.py
@@ -3,13 +3,6 @@ import sys
 values = [57, 71, 87, 97, 99, 101, 103, 113, 114, 115, 128, 129, 131, 137, 147, 156, 163, 186]
 
 def count_peptides(mass):
-
-	"""
-	Runtime ?
-	@param mass - target peptide mass
-	@return count of all linear peptides of mass "mass"
-	"""
-
 	table = [{"count": 1, "combos": [[]]}]
 	for i in xrange(mass):
 		table.append({"count": 0, "combos": []})
@@ -18,17 +11,10 @@ def count_peptides(mass):
 			if table[i-value]['count'] != 0:
 				table[i]['count'] += table[i-value]['count']
 				for combo in table[i-value]['combos']:
-					table[i]['combos'].append(list(combo) + [value]) # This is the problem! Copying lists is expensive
-	return reduce(lambda x,y: x+y, map(count_all_unique_orderings, table[mass]['combos']), 0)
+					table[i]['combos'].append(combo + [value]) # This is the problem! Copying lists is expensive
+	return sum(map(count_all_unique_orderings, table[mass]['combos']))
 
 def count_all_unique_orderings(nums):
-
-	"""
-	Runtime O(|nums|^2)
-	@param nums - list of sorted numbers
-	@return count of unique permutations of nums
-	"""
-
 	freq_counts = get_freq_counts(nums)
 	total = len(nums)
 	solution = 1
@@ -38,13 +24,6 @@ def count_all_unique_orderings(nums):
 	return solution
 
 def get_freq_counts(nums):
-
-	"""
-	Runtime O(|nums|)
-	@param nums - list of sorted numbers
-	@return list of how frequently each unique number appears
-	"""
-
 	ptr = 0
 	freq_counts = []
 	count = 1
@@ -60,14 +39,6 @@ def get_freq_counts(nums):
 	return freq_counts
 
 def count_n_choose_k(N, k):
-
-	"""
-	Runtime O(N)
-	@param N - integer, length of a set
-	@param k - integer <= N
-	@return length of n_choose_k
-	"""
-
 	if (k > N) or (N < 0) or (k < 0):
 		return 0
 	val = 1
