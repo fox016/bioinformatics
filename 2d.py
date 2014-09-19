4bc2b2e56a5e21148bdd6ffd9d29ac96a7eca80c
Nate Fox <nfox@byu.edu>
2014-09-19 16:55:19 -0600

    working on 2d but still memory problems

--- 2d.py
+++ 2d.py
@@ -2,6 +2,41 @@ import sys
 
 values = [57, 71, 87, 97, 99, 101, 103, 113, 114, 115, 128, 129, 131, 137, 147, 156, 163, 186]
 
+"""
+def count_peptides_new(mass):
+	table = [{"count": 1, "pairs": [], "index": 0}]
+	for i in xrange(mass):
+		table.append({"count": 0, "pairs": [], "index": i+1})
+	for value in values:
+		for i in xrange(value, mass+1):
+			if table[i-value]['count'] != 0:
+				table[i]['count'] += table[i-value]['count']
+				table[i]['pairs'].append((i-value,value))
+	combos = build_combos(table, mass)
+	return sum(map(count_all_unique_orderings, combos))
+
+combo_map = {0: [[]]}
+for value in values:
+	combo_map[value] = [[value]]
+
+def build_combos(table, index):
+
+	if index in combo_map:
+		return combo_map[index]
+
+	pairs = table[index]['pairs']
+	combos = []
+	for i in xrange(len(pairs)):
+		left = build_combos(table, pairs[i][0])
+		right = build_combos(table, pairs[i][1])
+		for left_list in left:
+			for right_list in right:
+				combos.append(left_list + right_list)
+	
+	combo_map[index] = combos
+	return combos
+"""
+
 def count_peptides(mass):
 	table = [{"count": 1, "combos": [[]]}]
 	for i in xrange(mass):
@@ -46,5 +81,5 @@ def count_n_choose_k(N, k):
 		val = (val*(N-j))//(j+1)
 	return val
 
-mass = 1024
+mass = 1359
 print count_peptides(mass)
