76fc36bdfff157df1b170c16c0994a7a8a4c8ca2
Nathan Fox <fox016@gmail.com>
2014-09-30 15:40:02 -0600

    4i passoff try

--- 4i.py
+++ 4i.py
@@ -84,7 +84,26 @@ def reconstruct(d, reads):
 	for read in reads:
 		graph.add_edge(prefix(read), suffix(read))
 	path = graph.eulerian_path()
-	return path
+	node_size = (len(reads[0])-1) / 2 - 1
+	string = ["-"] * (len(reads[0])-1+d + len(reads) - 1)
+	for i in xrange(len(path)):
+		node = path[i].split("|")
+		string[i:i+node_size] = node[0]
+		string[i+d+node_size+1:i+d+node_size+node_size+1] = node[1]
+	return ''.join(string)
+
+"""
+def paired_composition(k, d, text):
+       composition = []
+       for index in xrange(len(text) - (2 * k + d) + 1):
+	       composition.append(text[index:index+k] + "|" + text[index+k+d:index+k+k+d])
+       return composition
+k = 3
+d = 1
+text = "TAATGCCATGGGATGTT"
+reads = paired_composition(k, d, text)
+print reconstruct(d, reads)
+"""
 
 read = [line[:-1] for line in open("input.txt", "r")]
 d = int(read[0])
