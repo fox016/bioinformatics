e36bbdacfd469e746f97ab75d07c99ebdabe858e
Nate Fox <nfox@byu.edu>
2014-09-30 17:16:14 -0600

    did 4j

--- 4j.py
+++ 4j.py
@@ -3,32 +3,36 @@
 class Graph:
 
 	def __init__(self):
-		self.edges = set()
 		self.node_edge_head_map = {}
 		self.node_edge_tail_map = {}
-		self.cycle = []
-		self.start_nodes = set()
-		self.extra_edge = None
 	
 	def add_edge(self, n1, n2):
 		edge = (n1, n2)
-		self.edges.add(edge)
 		if n1 in self.node_edge_head_map:
-			self.node_edge_head_map[n1].add(edge)
+			self.node_edge_head_map[n1].append(edge)
 		else:
-			self.node_edge_head_map[n1] = set([edge])
+			self.node_edge_head_map[n1] = [edge]
 		if n2 in self.node_edge_tail_map:
-			self.node_edge_tail_map[n2].add(edge)
+			self.node_edge_tail_map[n2].append(edge)
 		else:
-			self.node_edge_tail_map[n2] = set([edge])
+			self.node_edge_tail_map[n2] = [edge]
 
 	def get_contigs(self):
-		return map(self._get_contif_helper, self._get_branching_head_nodes())
+		contigs = map(lambda node: self._get_contig_helper(node), self._get_branching_head_nodes())
+		strings = []
+		for list_of_list_of_strs in contigs:
+			for list_of_strs in list_of_list_of_strs:
+				strings.append(list_of_strs[0] + ''.join(map(lambda x: x[-1], list_of_strs[1:])))
+		return strings
 
-	def _get_contig_helper(self, node):
-		if not self._is_branching(node):
-			return node
-		return node #TODO
+	def _get_contig_helper(self, node, path=[]):
+		if self._is_branching(node) and len(path) > 0:
+			return [path + [node]]
+		paths = []
+		for edge in self.node_edge_head_map[node]:
+			next_node = edge[1]
+			paths += self._get_contig_helper(next_node, path + [node])
+		return paths
 
 	def _get_branching_head_nodes(self):
 		return filter(self._is_branching, self.node_edge_head_map.keys())
@@ -36,7 +40,7 @@ class Graph:
 	def _is_branching(self, node):
 		if node not in self.node_edge_head_map or node not in self.node_edge_tail_map:
 			return True
-		elif len(self.node_edge_head_map[node]) != 1 or len(self.node_edge_tail_map[node]) != 1:
+		if len(self.node_edge_head_map[node]) != 1 or len(self.node_edge_tail_map[node]) != 1:
 			return True
 		return False
 
@@ -44,4 +48,4 @@ edge_input = [line[:-1] for line in open("input.txt", "r")]
 graph = Graph()
 for e in edge_input:
 	graph.add_edge(e[0:-1], e[1:])
-print graph.get_contigs()
+print ' '.join(graph.get_contigs())
