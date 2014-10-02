a0ff6f6b75e6f2154da0f6a95fd1dea2a84905e0
Nate Fox <nfox@byu.edu>
2014-10-02 13:44:21 -0600

    did 5f

--- 5f.py
+++ 5f.py
@@ -3,12 +3,13 @@
 DELETE = 0
 INSERT = 1
 MATCH = 2
+FREE = 3
 
 INDEL_COST = -5
 
 PROTEIN_INDEX_MAP = {'A': 0, 'C': 1, 'E': 3, 'D': 2, 'G': 5, 'F': 4, 'I': 7, 'H': 6, 'K': 8, 'M': 10, 'L': 9, 'N': 11, 'Q': 13, 'P': 12, 'S': 15, 'R': 14, 'T': 16, 'W': 18, 'V': 17, 'Y': 19}
 
-def local_alignment(v, w, matrix):
+def max_alignment(v, w, matrix):
 	table = []
 	ops = []
 	for i in xrange(len(v)+1):
@@ -22,7 +23,8 @@ def local_alignment(v, w, matrix):
 		for j in xrange(1, len(w)+1):
 			table[i][j], ops[i][j] = max_index([table[i-1][j] + INDEL_COST, \
 					table[i][j-1] + INDEL_COST, \
-					table[i-1][j-1] + match_cost(v[i-1], w[j-1], matrix)])
+					table[i-1][j-1] + match_cost(v[i-1], w[j-1], matrix), \
+					0])
 	print table[len(v)][len(w)]
 	unwind_ops(ops, v, w, len(v), len(w))
 
@@ -38,11 +40,14 @@ def unwind_ops(ops, v, w, i, j):
 			new_w.append(w[j-1])
 			j-=1
 			new_v.append("-")
-		else:
+		elif ops[i][j] == MATCH:
 			new_v.append(v[i-1])
 			new_w.append(w[j-1])
 			i-=1
 			j-=1
+		else:
+			i=0
+			j=0
 	print ''.join(new_v[::-1])
 	print ''.join(new_w[::-1])
 
@@ -54,10 +59,8 @@ def max_index(nums):
 	for index in xrange(1, len(nums)):
 		if nums[index] > best[0]:
 			best = (nums[index], index)
-	if best[0] < 0:
-		return (0, best[1])
 	return best
 
 matrix = [map(int, line.split()) for line in open("pam250.txt", "r")]
 v, w = [line[:-1] for line in open("input.txt", "r")]
-local_alignment(v, w, matrix)
+max_alignment(v, w, matrix)
