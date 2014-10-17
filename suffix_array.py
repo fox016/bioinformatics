5d36e1ede83e335ce3aa7e87526907bb8c2b7a6d
Nate Fox <nfox@byu.edu>
2014-10-16 18:02:48 -0600

    suffix array can do small, not large

--- suffix_array.py
+++ suffix_array.py
@@ -3,6 +3,7 @@
 # http://www.cs.helsinki.fi/u/tpkarkka/publications/jacm05-revised.pdf
 # http://en.wikipedia.org/wiki/Radix_sort#Example_in_Python
 
+"""
 text = "yabbadabbado"
 alpha = "$abdoy"
 alpha_index_map = {
@@ -13,7 +14,37 @@ alpha_index_map = {
 	"o": 4,
 	"y": 5
 }
+"""
+
+text = [line[:-2] for line in open("input.txt", "r")][0]
+alpha = "$ACGT"
+alpha_index_map = {
+	"$": 0, 
+	"A": 1,
+	"C": 2,
+	"G": 3,
+	"T": 4
+}
+
 radix = 10
+
+def radix_sort(list_of_nums):
+	solution = list(list_of_nums)
+	digit_place = 1
+	done = False
+	while True:
+		done = True
+		buckets = [list() for _ in xrange(radix)]
+		for num in solution:
+			index = (num / digit_place)
+			buckets[index % 10].append(num)
+			if index > 0:
+				done = False
+		if done:
+			break
+		solution = reduce(lambda x,y: x+y, buckets, [])
+		digit_place*=10
+	return solution
 
 def get_prime(list_of_nums, sorted_list):
 	list_map = {}
@@ -30,20 +61,13 @@ def get_prime(list_of_nums, sorted_list):
 		list_map[element]['count']-=1
 	return ranks
 
-def radix_sort(list_of_nums):
-	solution = list(list_of_nums)
-	placement = 1
-	while True:
-		buckets = [list() for _ in xrange(radix)]
-		for num in solution:
-			index = num / placement
-			buckets[index % radix].append(num)
-		print "buckets", buckets
-		if index <= 0:
-			break
-		solution = reduce(lambda x,y: x+y, buckets, [])
-		placement*=radix
-	return solution
+def compare_tuple(t1, t2):
+	for index in xrange(len(t1)):
+		if t1[index] < t2[index]:
+			return -1
+		if t1[index] > t2[index]:
+			return 1
+	return 0
  
 def suffix_array(T):
 
@@ -56,9 +80,6 @@ def suffix_array(T):
 	R = map(int, [T_int[k+start:k+start+3] for k in [1,2] for start in xrange(0, n, 3)])
 	R_sorted = radix_sort(R)
 	R_prime = get_prime(R, R_sorted)
-	print "R", R
-	print "R_sorted", R_sorted
-	print "R_prime", R_prime
 
 	rank = [None for _ in xrange(len(T)+1)]
 	for index in xrange(len(C)):
@@ -66,13 +87,34 @@ def suffix_array(T):
 		rank[i] = R_prime[index]
 	rank[n+1] = 0
 	rank[n+2] = 0
-	print "Rank", rank
 
 	pairs = [int(T_int[i] + str(rank[i+1])) for i in B[0]]
 	pairs_sorted = radix_sort(pairs)
-	pairs_prime = get_prime(pairs, pairs_sorted)
-	print "Pairs", pairs
-	print "Pairs_sorted", pairs_sorted
-	print "Pairs_prime", pairs_prime
 
-suffix_array(text)
+	SC = map(lambda i: C[i-1], get_prime(R_sorted, R)) # TODO
+	SB = map(lambda i: B[0][i-1], get_prime(pairs_sorted, pairs))
+
+	solution = [0] * (n+1)
+	SC_index = 0
+	SB_index = 0
+	for solution_index in xrange(len(solution)):
+		i = SC[SC_index]
+		j = SB[SB_index]
+		if (i-1) % 3 == 0:
+			Si = (T_int[i], rank[i+1])
+			Sj = (T_int[j], rank[j+1])
+		else:
+			Si = (T_int[i], T_int[i+1], rank[i+2])
+			Sj = (T_int[j], T_int[j+1], rank[j+2])
+		if compare_tuple(Si, Sj) < 0:
+			solution[solution_index] = i
+			SC_index+=1
+			if SC_index >= len(SC):
+				return solution[0:solution_index+1] + SB[SB_index:]
+		else:
+			solution[solution_index] = j
+			SB_index+=1
+			if SB_index >= len(SB):
+				return solution[0:solution_index+1] + SC[SC_index:]
+
+print ', '.join(map(str, suffix_array(text)))
