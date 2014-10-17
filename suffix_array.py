6120c25e728bc7d430cc5471a072ff3949f65ca1
Nate Fox <nfox@byu.edu>
2014-10-17 16:34:14 -0600

    DONE WITH SUFFIX ARRAY

--- suffix_array.py
+++ suffix_array.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python
 
-# http://www.cs.helsinki.fi/u/tpkarkka/publications/jacm05-revised.pdf
-# http://en.wikipedia.org/wiki/Radix_sort#Example_in_Python
-
 """
-text = "yabbadabbado"
-alpha = "$abdoy"
-alpha_index_map = {
-	"$": 0, 
-	"a": 1,
-	"b": 2,
-	"d": 3,
-	"o": 4,
-	"y": 5
-}
+This is a linear-time implementation developed by Karkkainen, Sanders, and Burkhardt.
+See http://www.cs.helsinki.fi/u/tpkarkka/publications/jacm05-revised.pdf
+The paper provides C++ code. The following Python source code is entirely my own.
+
+Author - Nathan Fox
 """
 
 text = [line[:-2] for line in open("input.txt", "r")][0]
-text = "AACGATAGCGGTAGA"
 alpha = "$ACGT"
 alpha_index_map = {
 	"$": 0, 
@@ -27,23 +18,7 @@ alpha_index_map = {
 	"T": 4
 }
 
-def radix_sort(list_of_nums):
-	solution = list(list_of_nums)
-	digit_place = 1
-	done = False
-	while not done:
-		done = True
-		buckets = [list() for _ in xrange(10)]
-		for num in solution:
-			index = (num / digit_place)
-			buckets[index % 10].append(num)
-			if index > 0:
-				done = False
-		solution = reduce(lambda x,y: x+y, buckets, [])
-		digit_place*=10
-	return solution
-
-def get_prime(list_of_nums, sorted_list):
+def get_prime(list_of_tuples, sorted_list):
 	list_map = {}
 	duplicates = False
 	for index in xrange(len(sorted_list)):
@@ -52,7 +27,7 @@ def get_prime(list_of_nums, sorted_list):
 			duplicates = True
 		list_map[element] = index
 	ranks = []
-	for element in list_of_nums:
+	for element in list_of_tuples:
 		ranks.append(list_map[element]+1)
 	return ranks, duplicates
 
@@ -65,38 +40,23 @@ def compare_tuple(t1, t2):
 	return 0
  
 def suffix_array(T):
-
-	print "T", T
-
 	n = len(T)
 	B = [range(start, n+1, 3) for start in xrange(3)]
 	C = B[1] + B[2]
-	print "B", B
-	print "C", C
-
-	T += "00"
+	T += [0,0]
 	R = []
 	for k in [1,2]:
 		offset = 0
 		while k+offset <= B[k][-1]:
-			R.append(int(T[k+offset:k+offset+3]))
+			R.append(tuple(T[k+offset:k+offset+3]))
 			offset += 3
-	R_sorted = radix_sort(R)
+	R_sorted = sorted(R)
 	R_prime, is_dup = get_prime(R, R_sorted)
-	print "R", R
-	print "R_sorted", R_sorted
-	print "R_prime", R_prime
-
 	while is_dup:
-		SA_R = suffix_array(''.join(map(str, R_prime))) # TODO This only works if all elements of R_prime < 10
-		print "SA_R", SA_R
-		SA_R_sorted = radix_sort(SA_R)
-		print "SA_R_sorted", SA_R_sorted
+		SA_R = suffix_array(R_prime)
+		SA_R_sorted = sorted(SA_R)
 		R_prime, is_dup = get_prime(SA_R_sorted, SA_R)
-		print "R_prime", R_prime
-		R_prime = map(lambda x: x-1, R_prime[0:-1]) # This should always cut off the trailing 0
-		print "R_prime", R_prime
-
+		R_prime = map(lambda x: x-1, R_prime[0:-1])
 	rank = [None for _ in xrange(len(T)+1)]
 	SC = [0] * len(C)
 	for index in xrange(len(C)):
@@ -106,17 +66,9 @@ def suffix_array(T):
 		SC[value-1] = i
 	rank[n+1] = 0
 	rank[n+2] = 0
-	print "rank", rank
-	print "SC", SC
-
-	pairs = [int(T[i] + str(rank[i+1])) for i in B[0]]
-	pairs_sorted = radix_sort(pairs)
-	print "pairs", pairs
-	print "pairs_sorted", pairs_sorted
-
-	SB = map(lambda i: B[0][i-1], get_prime(pairs_sorted, pairs)[0]) # Not sure get_prime will return what I need here, may have to call recursively
-	print "SB", SB
-
+	pairs = [(T[i], rank[i+1]) for i in B[0]]
+	pairs_sorted = sorted(pairs)
+	SB = map(lambda i: B[0][i-1], get_prime(pairs_sorted, pairs)[0])
 	solution = [0] * (n+1)
 	SC_index = 0
 	SB_index = 0
@@ -141,4 +93,4 @@ def suffix_array(T):
 				return solution[0:solution_index+1] + SC[SC_index:]
 	return solution
 
-print ', '.join(map(str, suffix_array(''.join(map(lambda a: str(alpha_index_map[a]), text)))))
+print ', '.join(map(str, suffix_array(map(lambda a: alpha_index_map[a], text))))
