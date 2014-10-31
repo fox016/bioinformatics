8e888168521070e9906ed70f734e0a85a4365270
Nate Fox <nfox@byu.edu>
2014-10-31 11:35:57 -0600

    overlap looking good

--- overlap_assembly.py
+++ overlap_assembly.py
@@ -30,23 +30,37 @@ def unwind_ops(ops, v, w, i, j, solution_cost):
 		return None
 	new_v = []
 	new_w = []
+	v_len = 0
+	w_len = 0
 	while i != 0 or j != 0:
 		if ops[i][j] == DELETE:
 			new_v.append(v[i-1])
+			v_len+=1
 			i-=1
 			new_w.append("-")
 		elif ops[i][j] == INSERT:
 			new_w.append(w[j-1])
+			w_len+=1
 			j-=1
 			new_v.append("-")
 		elif ops[i][j] == MATCH:
 			new_v.append(v[i-1])
 			new_w.append(w[j-1])
+			v_len+=1
+			w_len+=1
 			i-=1
 			j-=1
 		else:
 			i = 0
-	return {"v": ''.join(new_v[::-1]), "w": ''.join(new_w[::-1]), "cost": solution_cost}
+	v = ''.join(new_v[::-1])
+	w = ''.join(new_w[::-1])
+	overlap = ''
+	for i in xrange(len(v)):
+		if v[i] != '-':
+			overlap += v[i]
+		else:
+			overlap += w[i]
+	return {"overlap": overlap, "v_len": v_len, "w_len": w_len, "cost": solution_cost}
 
 def match_cost(p1, p2):
 	return MATCH_COST if p1 == p2 else MISMATCH_COST
@@ -76,10 +90,46 @@ def build_graph(reads):
 						cost_coordinate_map[entry['cost']] = [(i, j)]
 	return matrix, cost_coordinate_map
 
-def get_contigs(matrix, cost_coordinate_map):
-	pass # TODO
+def reduce_matrix(matrix, cost_coordinate_map):
+	row_overlap_map = {}
+	for cost in sorted(cost_coordinate_map.keys(), reverse=True):
+		for coor in cost_coordinate_map[cost]:
+			if matrix[coor[0]][coor[1]]:
+				if not path_exists(row_overlap_map, coor[1], coor[0]):
+					row_overlap_map[coor[0]] = ({'row': coor[0],
+								'col': coor[1],
+								'overlap': matrix[coor[0]][coor[1]]['overlap'],
+								'v_len': matrix[coor[0]][coor[1]]['v_len'],
+								'w_len': matrix[coor[0]][coor[1]]['w_len']})
+					for i in xrange(len(matrix)):
+						matrix[coor[0]][i] = None
+						matrix[i][coor[1]] = None
+	return row_overlap_map
+
+def path_exists(row_overlap_map, start, end):
+	current = start
+	while current in row_overlap_map:
+		current = row_overlap_map[current]['col']
+	return current == end
+
+def build_contigs(reads, row_overlap_map):
+	starts = set(row_overlap_map.keys())
+	rows = set(row_overlap_map.keys())
+	for row in rows:
+		starts.discard(row_overlap_map[row]['col'])
+	contigs = []
+	for start in starts:
+		contig = reads[row_overlap_map[start]['row']]
+		current = start
+		while current in rows:
+			element = row_overlap_map[current]
+			contig = contig[0:-1 * element['v_len']] + element['overlap'] + reads[element['col']][element['w_len']:]
+			current = element['col']
+		contigs.append(contig)
+	return contigs
 
 reads = get_input_reads(sys.argv[1])
 matrix, cost_coordinate_map = build_graph(reads)
-print matrix
-print cost_coordinate_map
+row_overlap_map = reduce_matrix(matrix, cost_coordinate_map)
+contigs = build_contigs(reads, row_overlap_map)
+print '\n'.join(contigs)
